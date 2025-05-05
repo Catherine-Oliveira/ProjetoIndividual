a19eb552d0a7decfd884dbcc1929446b8109d236
Catherine Oliveira <catherine.oliveira@sptech.school>
2025-05-05 20:16:23 -0300

    Primeiro commit do projeto

---
+++ aaaaa/home/home.js
@@ -0,0 +1,9 @@
+function AbrirMenu() {
+    var menu = document.getElementById('dropdownMenu');
+    if (menu.style.display == 'block') {
+      menu.style.display = 'none'; 
+    } else {
+      menu.style.display = 'block'; 
+    }
+  }
+
